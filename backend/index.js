107fb14d0e747b512bcaf7bbfe356f0ebc9c2d9b
davidm00 <dmoses2@nd.edu>
2018-11-01 18:40:32 -0400

    updated frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -5,46 +5,21 @@ const imageData = require('./imageData');
 // Define variables
 const app = express();
 
+// enable CORS on /list route only
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 // app.use(express.json());
 // Body
 
-app.get('/', (req, res) => {
+app.get('/route', (req, res) => {
     fs.readFile('imageData.json', (err, data) => {
-       if (err) console.error('There was an error,', err);
-       res.send(imageData.dog);
+        if (err) console.error('There was an error,', err);
+        res.send(imageData);
     });
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
-/*
-app.get('/write', (req, res) => {
-
-    fs.writeFile('message.json', data.message, (err) => {
-        if (err) console.log(err);
-        console.log("Written to file");
-    });
-
-   res.status(200).send("Message is written to message.txt");
-});
-
-app.get('/message', (req, res) => {
-    fs.readFile('message.json', (err, data) => {
-         if (err) console.error('There was an error', err);
-         res.send(data.toString());
-    });
-});
-
-app.put('/write/:string', (req, res) => {
-    data.message = req.params.string;
-    res.send("New text written");
-});
-
-app.delete('/message/delete', (req, res) => {
-   fs.writeFile('message.txt', "", (err) => {
-      if (err) console.log(err);
-   });
-   res.send("Text deleted");
-});
-*/
-
