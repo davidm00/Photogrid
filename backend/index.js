7695ce4a4e21675709b00f74184409f9b9dcf8de
Patrick Soga <psoga117@gmail.com>
2018-11-13 18:45:28 -0500

    Finished feature

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const fs = require('fs');
 const imageData = require('./imageData');
 const colors = require('./colors.json');
+const getColors = require('get-image-colors');
+const download = require('image-downloader');
 // Define variables
 const app = express();
 
@@ -29,5 +31,40 @@ app.get('/getColors', (req, res) => {
         res.send(colors);
     });
 });
+
+app.get('/favicon.ico', (req, res) => {
+    res.sendStatus(404);
+});
+
+app.get('/:url', (req, res) => {
+    let fname = "";
+    const url = decodeURI(req.params.url);
+    const options = {
+        url: url,
+        dest: 'C:\\Users\\Patrick Soga\\Documents\\git-repos\\Photogrid\\backend\\Photos'
+    };
+    download.image(options)
+        .then(({ filename, image }) => {
+            fname = filename;
+            console.log('File saved to ', fname);
+            getColors(fname, (err, colors) => {
+               if (err) console.log(err);
+               else {
+                   let rgb = colors.map((color) => color.rgb());
+                   res.send({
+                       "primary": rgb[0],
+                       "secondary": rgb[1],
+                       "tertiary": rgb[2],
+                       "url": options.url
+                   });
+               }
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
